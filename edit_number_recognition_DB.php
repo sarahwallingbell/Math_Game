11aec2b5afe55aa4471e9333189fcaa202d94296
esternellis <esternellis@pugetsound.edu>
2020-03-10 11:40:55 -0700

    Update edit_number_recognition_DB.php

--- edit_number_recognition_DB.php
+++ edit_number_recognition_DB.php
@@ -1,1 +1,42 @@
+<?php
+$question_and_answer;
 
+function generate_file(){
+//variable $myFile holds the number recognition database
+$myFile = fopen("number_recognition_DB.txt", "c");
+
+//loop generates the file
+for ($i=10; $i < 1000; $i++) { 
+	$str =$i."_";
+	fwrite($myFile, $str);
+	generate_question_and_answer($i));
+	fwrite($myFile, $question_and_answer)
+	}
+
+	fclose($myFile);
+}
+
+function generate_question_and_answer($num){
+	$str = $num;
+
+	$randomChar = $str[rand(0, strlen($str)-1)];
+
+	$question = "What place is the $randomChar in?_"
+
+	$pos = strpos($num, $randomChar);
+
+	if($pos == 0){
+		$answer="Ones_";
+	}
+	if($pos == 1){
+		$answer="Tens_";
+	}
+	if($pos == 2){
+		$answer="Hundreds_";
+	}
+	$question_and_answer=$question.$answer;
+
+}
+
+
+?>

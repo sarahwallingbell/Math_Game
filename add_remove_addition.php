f7312149277202764bd0d28e1d88788c0d0b226b
manyam686 <61163106+manyam686@users.noreply.github.com>
2020-04-14 23:10:11 -0700

    small changes to add/remove addition

--- add_remove_addition.php
+++ add_remove_addition.php
@@ -10,14 +10,14 @@
     <h2>Add/Remove Addition Problem</h2>
 
     First Number:
-    <input type="number" name="first number" id="number" placeholder="x" />
+    <input type="number" name="firstnumber" id="firstnumber" placeholder="x" />
     <br/><br/>
 
     Second Number:
-    <input type="number" name="second number" id="number" placeholder="x" /><br/><br/>
+    <input type="number" name="secondnumber" id="secondnumber" placeholder="x" /><br/><br/>
 
     Sum:
-    <input type="number" name = "sum" id = "number" placeholder="x"/><br/><br/>
+    <input type="number" name = "sum" id = "sum" placeholder="x"/><br/><br/>
 
     <input type="submit" name="add_problem" id="add_problem" value="Add Problem" />
     <input type="submit" name="remove_problem" id="remove_problem" value="Remove Problem" />
--- add_remove_addition_DB.php
+++ add_remove_addition_DB.php
@@ -1,14 +1,19 @@
 <?php
+require_once("menu_bar.php");
 echo "<pre>";
-$fileName = "addition_testing.txt";
+$fileName = "additionDB.txt";
+$myFilePath="./".$fileName;
 $myFile = fopen(getcwd()."/".$fileName, "a+");
-$fileContents = file_get_contents($myFile, NULL);
+$fileContents = file_get_contents($myFilePath, NULL);
 
 //variable $myFile holds the addition database
 
 
 extract($_POST);
-$line = "".$firstnumber." ".$secondnumber." ".$sum." ";
+$firstnumber=$_POST['firstnumber'];
+$secondnumber=$_POST['secondnumber'];
+$sum=$_POST['sum'];
+$line = "".$firstnumber." ".$secondnumber." ".$sum."\n";
 if (isset ($_POST['add_problem'])){
 	if ($firstnumber =="" or $secondnumber == "" or !isset($sum)){
 		echo "Incomplete problem information, please fill all fields and try again.";
@@ -17,14 +22,16 @@ if (isset ($_POST['add_problem'])){
 	//if we get here, that means that all fields are entered
 	elseif (strpos($fileContents, ($line)) !== false){
 		echo "Problem already exists in problem set!";
+		header("refresh:3; url=add_remove_addition.php");
 	}
 	//check to see if the numbers actually equal the sum
-	elseif ($firstnumber + $secondnumber !== $sum) {
-		echo "Error: the first two numbers do not equal the sum";
+	elseif ($firstnumber + $secondnumber != $sum) {
+		echo "Error: ".$firstnumber." + ".$secondnumber." does not equal ".$sum.".";
+		header("refresh:3; url=add_remove_addition.php");
 	}
 	else {
 		//add the line to the contents of the file
-		echo "success";
+		echo "Success!";
 		$fileContents .= $line;
 		//overwrite the old file
 		file_put_contents($fileName, $fileContents);

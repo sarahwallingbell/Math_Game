15e00617f049aec4e76665af17da2e4050e1d5d9
SaraJaneG <49454754+SaraJaneG@users.noreply.github.com>
2020-04-14 21:23:32 -0700

    addition/subtraction updates
    updates to mimi and i's add/remove problem code for addition and subtraction.

--- add_remove_addition_DB.php
+++ add_remove_addition_DB.php
@@ -1,6 +1,6 @@
 <?php
 echo "<pre>";
-$fileName = "subtraction_testing.txt";
+$fileName = "addition_testing.txt";
 $myFile = fopen(getcwd()."/".$fileName, "a+");
 $fileContents = file_get_contents($myFile, NULL);
 
@@ -8,9 +8,9 @@ $fileContents = file_get_contents($myFile, NULL);
 
 
 extract($_POST);
-$line = "".$firstnumber." ".$secondnumber." ".$difference." ";
+$line = "".$firstnumber." ".$secondnumber." ".$sum." ";
 if (isset ($_POST['add_problem'])){
-	if ($firstnumber =="" or $secondnumber == "" or !isset($difference)){
+	if ($firstnumber =="" or $secondnumber == "" or !isset($sum)){
 		echo "Incomplete problem information, please fill all fields and try again.";
 		header("refresh:3; url=add_remove_addition.php");
 	}
@@ -18,21 +18,17 @@ if (isset ($_POST['add_problem'])){
 	elseif (strpos($fileContents, ($line)) !== false){
 		echo "Problem already exists in problem set!";
 	}
-	//all fields entered, but result is negative
-	elseif ($firstnumber - $secondnumber < 0) {
-		echo "Sorry, only problems with a positive result are allowed.";
+	//check to see if the numbers actually equal the sum
+	elseif ($firstnumber + $secondnumber !== $sum) {
+		echo "Error: the first two numbers do not equal the sum";
 	}
-	//all fields entered, but number1-number2 != difference
-	elseif ($firstnumber - $secondnumber !== $difference){
-		echo "Error: ".$firstnumber."- ".$secondnumber." does not equal ".$difference.".";
-	}
-	else{
+	else {
 		//add the line to the contents of the file
 		echo "success";
 		$fileContents .= $line;
 		//overwrite the old file
 		file_put_contents($fileName, $fileContents);
-	}		
+	}
 }
 elseif (isset($_POST['remove_problem'])){
 	if ($firstnumber =="" or $secondnumber == "" or !isset($sum)){
@@ -43,7 +39,7 @@ elseif (isset($_POST['remove_problem'])){
 	elseif (strpos($fileContents, ($line)) == false){
 		echo "Problem does not exist in problem set, please try again.";
 	}
-	//if we get here, we should remove it.
+	//if we get
 	else {
 		//get rid of the line
 		//str_replace($line, "", $fileContents, 1);

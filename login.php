7b6fa00dc1fba89d250010cb0ed6bf8c7fe2fb8c
manyam686 <61163106+manyam686@users.noreply.github.com>
2020-05-08 15:22:14 -0700

    added colors to login page

--- login.php
+++ login.php
@@ -3,25 +3,52 @@
 <head>
 	<meta charset="utf-8">
 	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
 	<!-- Include JS File Here -->
 	<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
 	</head>
 <body>
-	<form action="check_login.php" method="post" id="form_id">
+	<style type="text/css">
+	body{
+		background-color:  #F7DAD4;
+	}
+	h2{
+		font-family: Impact;
+		font-size: 50px;
+	}
+	.box{
+		width: 200px;
+		height: 30px;
+		font-family: Arial;
+		font-size: 25px;
+		background-color: #236AB9;
+	}
+	.label{
+		font-family: Arial;
+		font-size: 25px;
+	}
+	#login_info{
+		text-align: center;
+	}
+	#checkbox{
+		background-color: #FD3A0F
+	}
+	</style>
+	<form action="check_login.php" method="post" id="login_info">
 		<h2>Welcome to Math Game</h2>
-		Userame:
-		<input type="text" name="username" id="username" placeholder="Name" />
+		<tag class="label">Userame:</tag>
+		<input type="text" name="username" id="username" class="box" placeholder="Name" />
 		<br/><br/>
-		Password:
-		<input type="password" name="password" id="password" placeholder="Password" /><br/><br/>
+		<tag2 class="label">Password:</tag2>
+		<input type="password" name="password" id="password" class="box" placeholder="Password" /><br/><br/>
 
-		<input type="radio" name="account_type" id="teacher" value="teacher">
-		<label for="teacher">Teacher</label>
-		<input type="radio" name="account_type" id="student"  value="student">
-		<label for="student">Student</label><br><br>
+		<input type="radio" name="account_type" id="checkbox" value="teacher">
+		<label for="teacher" class="label">Teacher</label>
+		<input type="radio" name="account_type" id="checkbox"  value="student">
+		<label for="student" class="label">Student</label><br><br>
 
-		<input type="submit" name="login" id="login" value="Login" />
-		<input type="submit" name="create_account" id="create_account" value="Create Account" />
+		<input type="submit" name="login" id="button" value="Login" />
+		<input type="submit" name="create_account" id="button" value="Create Account" />
 
 
 	</form>
